89e643b5a39b8895fd2fff664df699bbf8a1faf5
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-06-25 16:25:10 +0300

    Linking register api

--- address-book-frontend/src/pages/Signup.js
+++ address-book-frontend/src/pages/Signup.js
@@ -1,11 +1,47 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 
 const Signup = () => {
 
-    function signupFunction() {
-        document.location.href = "./home";
+    async function signupFunction() {
+        var name = document.getElementById("su_name").value;
+        var email = document.getElementById("su_email").value;
+        var password = document.getElementById("su_password").value;
+        var confirm_password = document.getElementById("su_confirm_password").value;
+        if (name == "" || email == "" || password == "" || confirm_password == "") {
+            alert('Fill All')
+        } else {
+            if (password == confirm_password) {
+
+                const params = new URLSearchParams();
+                params.append('fullName', name);
+                params.append('email', email);
+                params.append('password', password);
+
+                // const params = new URLSearchParams();
+                // params.append('email', email);
+                // params.append('password', password);
+                await axios.post(`http://localhost:3000/auth/register`, params, { 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' }).then(res => {
+                    // if login
+                    if (res['status'] == 200) {
+                        console.log('noice');
+                        // save access token in local storage for later use
+                        // localStorage.setItem('token', res.data['token'])
+                        // document.location.href = "./home";
+                    }
+                })
+                    .catch(err => {
+                        alert(err.response.data['message']);
+                    })
+
+            }
+            else {
+                alert('Passwords dont match!')
+            }
+        }
+
     }
 
     return (
