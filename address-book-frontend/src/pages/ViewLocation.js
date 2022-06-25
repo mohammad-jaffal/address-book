a95031f2b9d99e8420803d57690164adafd26514
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-06-25 04:37:47 +0300

    Creating map page

--- address-book-frontend/src/pages/ViewLocation.js
+++ address-book-frontend/src/pages/ViewLocation.js
@@ -1,22 +1,45 @@
-import {React, useMemo} from "react";
+import { React, useMemo } from "react";
 import Navbar from "../components/Navbar";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
 
 
 const ViewLocation = () => {
+    const { isLoaded } = useLoadScript({ googleMapsApiKey: "AIzaSyBS8VZXIVu9EOghzUMzzI9kwxq92G7Ckb4" });
 
-
+    if (!isLoaded) {
+        return (
+            <div className='global-container'>
+                <Navbar />
+                <div>Loading...</div>
+            </div>
+        );
+    }
 
     return (
-        <div className='global-container'>
-            <Navbar/>
-            <div className="viewmap-body-container"> 
-            MAP
-            </div>
-        </div>
+        <Map />
     );
 };
 
-
 export default ViewLocation;
+
+
+
+function Map() {
+    return (
+        <div className='global-container'>
+            <Navbar />
+            <div className="viewmap-body-container">
+                <GoogleMap
+                    zoom={10}
+                    center={{ lat: 33.888630, lng: 35.422281 }}
+                    mapContainerClassName="map-container"
+                >
+                    <Marker
+                        position={{ lat: 33.888630, lng: 35.822281 }}
+                    />
+                </GoogleMap>
+            </div>
+        </div>
+    );
+}
